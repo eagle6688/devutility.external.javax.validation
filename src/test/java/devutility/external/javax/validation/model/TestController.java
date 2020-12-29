445caf4c5bef3a9633dfdbfb6da28ab9dd76fd11
Aldwin Su <fangyansu@163.com>
2020-12-29 14:43:11 +0800

    Update

--- src/test/java/devutility/external/javax/validation/model/TestController.java
+++ src/test/java/devutility/external/javax/validation/model/TestController.java
@@ -5,11 +5,11 @@ import java.util.List;
 import javax.validation.Valid;
 
 import devutility.external.javax.validation.annotation.PatternIfNotBlank;
-import devutility.internal.model.OperationResult;
+import devutility.internal.response.EasyResponse;
 import devutility.internal.security.RegExpUtils;
 
 public class TestController {
-	public OperationResult test(@PatternIfNotBlank(regexp = RegExpUtils.IP, message = "Invalid IP address") String ip, @Valid User user, @Valid List<User> users) {
-		return new OperationResult();
+	public EasyResponse test(@PatternIfNotBlank(regexp = RegExpUtils.IP, message = "Invalid IP address") String ip, @Valid User user, @Valid List<User> users) {
+		return new EasyResponse();
 	}
 }
